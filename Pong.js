fb501182b66a6ff5494b44526ae5f823dd867e06
Matheus Malveira <168776437+MatheusMalveira@users.noreply.github.com>
2024-06-27 17:41:26 -0300

    Cria Bolinha

--- Pong.js
+++ Pong.js
@@ -1,3 +1,10 @@
+//aqui vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
+
+
 function setup() {
     // Aqui vou criar minha "mesa"
    createCanvas(600,400);
@@ -6,4 +13,16 @@ function setup() {
 function draw() {
     // Aqui vou colocar a cor da "mesa"
     background(139,0,0);
+    //chamando a função
+    CriaBolinha(xBolinha, yBolinha, tamBolinha);
 }
+
+//função bolinha
+function CriaBolinha(xBolinha, yBolinha, tamBolinha) {
+    circle(xBolinha, yBolinha, tamBolinha);
+
+}
+
+
+
+
